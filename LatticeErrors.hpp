47e2734427969d6232bb1763f54d192ff86c4c33
LangLiuUCSB <157067894+LangLiuUCSB@users.noreply.github.com>
2024-11-25 17:49:39 -0800

    rename from Errors to LatticeErrors

--- LatticeErrors.hpp
+++ LatticeErrors.hpp
@@ -1,11 +1,11 @@
-#ifndef ERRORS_H
-#define ERRORS_H
+#ifndef LATTICEERRORS_H
+#define LATTICEERRORS_H
 
 #include "Coordinate.hpp"
 
 class InvalidSource : public std::exception
 {
-    std::string message;
+    const std::string message;
 
 public:
     explicit InvalidSource(const Coordinate &coordinate) noexcept
@@ -15,7 +15,7 @@ public:
 
 class InvalidTarget : public std::exception
 {
-    std::string message;
+    const std::string message;
 
 public:
     explicit InvalidTarget(const Coordinate &coordinate) noexcept
@@ -25,7 +25,7 @@ public:
 
 class Untraversable : public std::exception
 {
-    std::string message;
+    const std::string message;
 
 public:
     explicit Untraversable(Coordinate &source, Coordinate &target) noexcept
@@ -35,11 +35,21 @@ public:
 
 class InvalidSearchMode : public std::exception
 {
-    std::string message;
+    const std::string message;
 
 public:
     explicit InvalidSearchMode(char enum_constant) noexcept
         : message("Invalid enum constant " + std::string(1, enum_constant)) {}
+    const char *what() const noexcept override { return message.c_str(); }
+};
+
+class InvalidRoute : public std::exception
+{
+    const std::string message;
+
+public:
+    explicit InvalidRoute(char move, size_t move_index) noexcept
+        : message("Invalid route with impossible move \'" + std::string(1, move) + "\' at index " + std::to_string(move_index)) {}
     const char *what() const noexcept override { return message.c_str(); }
 };
 
