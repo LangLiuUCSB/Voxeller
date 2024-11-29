c9342fa190306146faaa66078c9246f05c4e3576
LangLiuUCSB <157067894+LangLiuUCSB@users.noreply.github.com>
2024-11-29 00:07:28 -0800

    Update Coordinate.hpp

--- Coordinate.hpp
+++ Coordinate.hpp
@@ -21,6 +21,18 @@ struct Coordinate
   {
     return x != other.x || y != other.y || z != other.z;
   }
+  friend size_t manhattan_distance(const Coordinate &c1, const Coordinate &c2) noexcept
+  {
+    return abs(c1.x - c2.x) + abs(c1.y - c2.y) + abs(c1.z - c2.z);
+  }
+  std::string to_string() const noexcept
+  {
+    return "(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + ")";
+  }
+  friend std::ostream &operator<<(std::ostream &s, const Coordinate &c) noexcept
+  {
+    return s << c.to_string();
+  }
   Coordinate east() const noexcept { return Coordinate(x + 1, y, z); }
   Coordinate west() const noexcept { return Coordinate(x - 1, y, z); }
   Coordinate south() const noexcept { return Coordinate(x, y + 1, z); }
@@ -28,20 +40,8 @@ struct Coordinate
   Coordinate up() const noexcept { return Coordinate(x, y, z + 1); }
   Coordinate down() const noexcept { return Coordinate(x, y, z - 1); }
   void fall() noexcept { --z; }
-  std::string to_string() const noexcept
-  {
-    return "(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + ")";
-  }
 };
 
-inline std::ostream &operator<<(std::ostream &s, const Coordinate &c) noexcept
-{
-  return s << c.to_string();
-}
-inline size_t manhattan_distance(const Coordinate &c1, const Coordinate &c2) noexcept
-{
-  return abs(c1.x - c2.x) + abs(c1.y - c2.y) + abs(c1.z - c2.z);
-}
 struct CoordinateHash
 {
   std::size_t operator()(const Coordinate &c) const noexcept
